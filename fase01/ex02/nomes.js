be63c81eef4bd92196b4cc010429adcac1235970
kellyzikadack2 <59873617+kellyzikadack2@users.noreply.github.com>
2020-01-14 11:53:34 -0300

    1aula

---
+++ fase01/ex02/nomes.js
@@ -0,0 +1,24 @@
+/*function mostrarNome ()  {
+     const nome = "kellyx";
+     console.log(nome)
+}
+
+mostrarNome()
+
+function nomeDeVolta(){
+    const name = "O android paranóide";
+    console.log(name);
+}
+
+nomeDeVolta()*/
+
+function nomes(nome1,  nome2){
+    const nick = "Arthur " + " e" + " Zaphod";
+     console.log(nick);
+}
+
+nomes()
+     
+     
+
+
