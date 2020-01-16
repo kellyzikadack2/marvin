2d3ebc7ea328cbea9f26bb6b44b4de52f6c7774e
kellyzikadack2 <59873617+kellyzikadack2@users.noreply.github.com>
2020-01-16 11:05:00 -0300

    licao2

--- fase01/ex02/nomes.js
+++ fase01/ex02/nomes.js
@@ -1,24 +1,5 @@
-/*function mostrarNome ()  {
-     const nome = "kellyx";
-     console.log(nome)
-}
-
-mostrarNome()
-
-function nomeDeVolta(){
-    const name = "O android paranóide";
-    console.log(name);
-}
-
-nomeDeVolta()*/
-
 function nomes(nome1,  nome2){
     const nick = "Arthur " + " e" + " Zaphod";
      console.log(nick);
 }
-
-nomes()
-     
-     
-
 
