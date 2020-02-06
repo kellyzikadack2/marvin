2208d56a824255b7cd61cf36f05b4c1879dc29d5
kellyzikadack2 <59873617+kellyzikadack2@users.noreply.github.com>
2020-02-06 11:22:32 -0300

    1aula

---
+++ fase04/ex01/ateZero.js
@@ -0,0 +1,15 @@
+function ateZero(numero){
+  var n = 0
+  if (numero>0){
+  while(n <= numero) {
+      console.log(n)
+      n++
+    }
+  }
+  if(numero<=0){
+  while(n>=numero){
+      console.log(numero)
+  numero++
+  }
+  }
+}
