ee67c26c2e563ad70aaf8a2655d709c5d129c17d
kellyzikadack2 <59873617+kellyzikadack2@users.noreply.github.com>
2020-02-13 09:15:11 -0300

    fase3

---
+++ fase04/ex03/por42.js
@@ -0,0 +1,16 @@
+function por42(num1, num2){
+    var result = null;
+    var contador = 0;
+    while ( num1 <= num2){
+        if ( num1 % 42 === 0){
+            contador = contador +1;
+        }
+        if (contador === 2){
+            return(num1);
+        }
+        num1++
+    } if (!result)
+    console.log("Não encontrado")
+    return false
+} 
+
